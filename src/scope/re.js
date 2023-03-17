15fd84514f686ad85c49835a42ec7c867658a8ca
Vidoalessandro <gabriellevido17@gmail.com>
2023-03-16 22:06:11 -0400

    Redeclaring and reassign

---
+++ src/scope/re.js
@@ -0,0 +1,34 @@
+var firstName; // Undefined
+firstName = 'Ale';
+console.log(firstName);
+
+var lastName = 'David'; // declaring / assign
+lastName = 'Anna'; // reassign
+console.log(lastName);
+
+var secondName = 'David'; // declaring / assign
+var secondName = 'Anna'; // reassign
+console.log(secondName);
+
+// Let
+
+let fruit = 'Apple'; // declaring and assign
+fruit = 'Kiwi'; // reassign
+console.log(fruit);
+
+// let fruit = 'Bannana';
+console.log(fruit);
+
+// Const
+
+const animal = 'dog'; // declaring and assign
+// animal = 'cat'; // reassign
+// const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('🚗');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
