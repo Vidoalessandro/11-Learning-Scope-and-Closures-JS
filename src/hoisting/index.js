3fbe66e06d002ad5c81e456e24ed54cad93e6317
Vidoalessandro <gabriellevido17@gmail.com>
2023-03-18 17:17:54 -0400

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,16 @@
+
+var nameOfDog; // undefined
+
+console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+console.log(nameOfDog);
+
+
+var elmo; // undefined
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`The best dog is ${elmo}`);
+}
+
+var elmo = 'Elmito';
